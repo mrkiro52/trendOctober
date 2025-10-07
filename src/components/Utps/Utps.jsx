c95fd6fc1864217f83ec8e65e4644864261706ec
Name <example@gmail.com>
2025-10-07 15:35:01 +0700

    added link

--- src/components/Utps/Utps.jsx
+++ src/components/Utps/Utps.jsx
@@ -29,7 +29,7 @@ export default function Utps() {
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 16 16"><title>Arrow-up-right-16-filled SVG Icon</title><path fill="black" d="M7 2.75A.75.75 0 0 1 7.75 2h5.5a.75.75 0 0 1 .75.75v5.5a.75.75 0 0 1-1.5 0V4.56l-9.22 9.22a.75.75 0 0 1-1.06-1.06l9.22-9.22H7.75A.75.75 0 0 1 7 2.75"/></svg>
                             <span>Еженедельные выплаты за оказание услуг</span>
                         </div>
-                        <button>Смотреть вакансии</button>
+                        <a href='https://spb.hh.ru/employer/4258986' target='_blank'>Смотреть вакансии</a>
                     </div>
                     <div className="right">
                         <img src={gendir} alt="gendir" />
